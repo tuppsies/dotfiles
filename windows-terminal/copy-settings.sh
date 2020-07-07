11f22dc052313e83419b4982236ad65a6729b8a2
tuppsies <tupperwarec@hotmail.com>
2020-07-07 21:49:01 +1000

    Fixed naming error

--- windows-terminal/copy-settings.sh
+++ windows-terminal/copy-settings.sh
@@ -1,2 +1,2 @@
-# Copies the settings.json file for Microsoft Terminal to the current directory
+# Copies the settings.json file for Windows Terminal to the current directory
 cp  /mnt/c/Users/Tuppe/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json settings.json
